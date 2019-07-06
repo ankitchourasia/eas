7ca0b8535782bc96067a239f64a2a3a64e4bd054
ank02 <ankit.chourasia@outlook.com>
2019-07-06 12:05:05 +0530

    proxy changes wip

--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { LoginRoutingModule } from './login-routing.module';
 import { FormsModule  } from '@angular/forms';
-import { LoginServiceModule } from 'src/app/services/login/login-service.module';
+import { LoginServiceModule } from '@eas-services/login/login-service.module';
 
 @NgModule({
   imports: [
--- src/app/services/login/login.service.ts
+++ src/app/services/login/login.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@angular/core';
-import { Http, Response, Headers } from '@angular/http';
+import { Http, Headers } from '@angular/http';
+import { HttpResponse } from "@angular/common/http";
+import {map} from 'rxjs/operators';
 
 @Injectable()
 export class LoginService {
 
   constructor(private http: Http) { }
 
-  AUTHENTICATION_URL : string = "/ROOT/backend/authentication/login";
+  AUTHENTICATION_URL : string = "/backend/authentication/login";
   public authenticate(user : any){
-    return this.http.get(this.AUTHENTICATION_URL,user);
+    let headers = new Headers();
+    headers.append('Authorization', 'Basic ' + btoa(user.username + ':' + user.password));
+    let options : any = { headers : headers};
+    options['observe'] = "response";
+    return this.http.get(this.AUTHENTICATION_URL, options);
   }
 }
