5c76e21eda1b881f59558472aeb0bfaf6a06b6f3
unknown <manishkushwaha07@gmail.com>
2019-07-08 12:15:01 +0530

    some new directive create

--- src/app/directives/directive.module.ts
+++ src/app/directives/directive.module.ts
@@ -1,21 +1,33 @@
 import { NgModule } from '@angular/core';
+import { LowercaseDirective } from './lowercase.directive';
 import { UppercaseDirective } from './uppercase.directive';
 import { MinValidatorDirective } from './min-validator.directive';
 import { MaxValidatorDirective } from './max-validator.directive';
 import { ScrollIntoViewDirective } from './scroll-into-view.directive';
+import { RemoveCharacterDirective } from './remove-character.directive';
+import { CompareValidatorDirective } from './compare-validator.directive';
+import { EqualValidatorDirective } from './equal-validator.directive';
 
 @NgModule({
   declarations: [
+    LowercaseDirective,
     UppercaseDirective,
     MinValidatorDirective,
     MaxValidatorDirective,
-    ScrollIntoViewDirective
+    ScrollIntoViewDirective,
+    RemoveCharacterDirective,
+    CompareValidatorDirective,
+    EqualValidatorDirective,
   ],
   exports:[
+    LowercaseDirective,
     UppercaseDirective,
     MinValidatorDirective,
     MaxValidatorDirective,
-    ScrollIntoViewDirective
+    ScrollIntoViewDirective,
+    RemoveCharacterDirective,
+    CompareValidatorDirective,
+    EqualValidatorDirective
   ],
 })
 export class DirectiveModule { }
