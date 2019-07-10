2c7e7e1f71e078b5d3b8e82e98d227e51517ecf2
unknown <manishkushwaha07@gmail.com>
2019-07-10 16:07:51 +0530

    feeder add and view complete

--- src/app/components/feeder/feeder-view/feeder-view.component.ts
+++ src/app/components/feeder/feeder-view/feeder-view.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { GlobalResources } from 'app/utility/global.resources';
+import { GlobalConstants } from 'app/utility/global.constants';
+import { PaginationService } from '@eas-services/pagination/pagination.service';
+import { FeederService } from '@eas-services/feeder/feeder.service';
+import { SubstationService } from '@eas-services/substation/substation.service';
+import $ from 'jQuery';
 
 @Component({
   selector: 'eas-feeder-view',
@@ -7,9 +13,131 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeederViewComponent implements OnInit {
 
-  constructor() { }
+  user : any;
+  feeders : any = [];
+  feederToEdit: any = {};
+  showModal : boolean;
+  pagedFeeders : any = [];
+  substationList: any;
+  pager: any = {};
+  pageSize: number;
+  @ViewChild('modalCloseButtonRef') modalCloseButtonRef: ElementRef;
+  constructor(private feederService : FeederService,  private substationService : SubstationService, 
+    private globalResources : GlobalResources, private paginationService : PaginationService) { }
 
   ngOnInit() {
+    this.user = this.globalResources.getUserDetails();
+    this.getFeeders();
+    this.initializePaginationVariables();
   }
+
+  initializePaginationVariables(){
+    this.pager = {};
+    this.pageSize = 10;
+  }
+
+  getFeeders(){
+    this.feederService.getFeederByDivisionId(this.user.division.id).subscribe(success =>{
+      console.log(success);
+      this.feeders = success;
+      this.setPage(1);
+    }, error =>{
+      console.log(error);
+    });
+  }
+
+  exportClicked(){
+    var params = {Authorization: 'Basic ' + sessionStorage.getItem('encodedCredentials')};
+    let fileUrl = null;
+    if(this.user.role === 'admin'){
+			fileUrl = GlobalConstants.URL_PREFIX_FOR_FILE_EXPORT + "export/feeder/division/" + this.user.division.id;
+		}else{
+			fileUrl = GlobalConstants.URL_PREFIX_FOR_FILE_EXPORT + "export/feeder/zone/" + this.user.zone.id;
+		}
+    var url = [fileUrl, $.param(params)].join('?');
+		window.open(url);
+  }
+
+  deleteButtonClicked(feeder){
+    let confirmAlertResponse : any = this.globalResources.confirmAlert("Are you sure to delete this feeder ?")
+    confirmAlertResponse.then((result) => {
+      if(result.value) {
+        this.deleteFeeder(feeder.id, this.user.username);
+      }else{
+        console.log("confirm alert else");
+      }
+    });
+  }
+
+  deleteFeeder(feederId, deletedBy){
+    console.log(feederId, deletedBy);
+    this.feederService.deleteFeederById(feederId, deletedBy).subscribe(success => {
+      let alertResponse = this.globalResources.successAlert("feeder deleted successfully");
+      alertResponse.then(result =>{
+        this.getFeeders();
+      });
+    }, error =>{
+      console.log(error);
+    });
+  }
+
+  
+  editButtonClicked(feeder){
+    this.showModal = true;
+    this.feederToEdit = Object.assign({}, feeder);
+    console.log(this.feederToEdit);
+    this.getSubstationByZoneId(this.feederToEdit.zoneId);
+  }
+
+  zoneChanged(){
+    console.log("zoneChanged");
+    this.substationList = null;
+    this.feederToEdit.substationId = undefined;
+    this.getSubstationByZoneId(this.feederToEdit.zoneId);
+  }
+
+  getSubstationByZoneId(zoneId){
+    this.substationService.getSubstationsByZoneId(zoneId).subscribe(succcess =>{
+      this.substationList = succcess;
+    }, error =>{
+      console.log(error);
+    });
+  }
+  
+  updateClicked(updateFeederForm){
+    if(this.globalResources.validateForm(updateFeederForm)){
+      this.feederService.updateFeeder(this.feederToEdit, this.user.username).subscribe(success =>{
+        let alertResponse = this.globalResources.successAlert("Feeder updated successfully");
+        alertResponse.then(result =>{
+          console.log("alert result", result);
+          this.closeModal(this.modalCloseButtonRef);
+          this.getFeeders();
+        });
+      }, error =>{
+        console.log(error);
+        let alertResponse = this.globalResources.errorAlert("Unable to update substation.");
+        alertResponse.then(result =>{
+          console.log("alert result", result);
+        });
+      })
+    }
+  }
+
+  setPage(page: number) {
+    console.log(page);
+    if (page < 1 || page > this.pager.totalPages) {
+      return;
+    }
+    this.pager = this.paginationService.getPager(this.feeders.length, page, this.pageSize);
+    console.log(this.pager);
+    this.pagedFeeders = this.feeders.slice(this.pager.startIndex, this.pager.endIndex + 1);
+    console.log(this.pagedFeeders);
+  }
+  
+  closeModal(modalCloseButtonRef: ElementRef){
+    console.log(modalCloseButtonRef);
+    modalCloseButtonRef.nativeElement.click();
+  }
+
 
 }
--- src/app/components/feeder/feeder.module.ts
+++ src/app/components/feeder/feeder.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { FeederAddComponent } from './feeder-add/feeder-add.component';
 import { FeederViewComponent } from './feeder-view/feeder-view.component';
-import { FormsModule } from '@angular/forms';
 import { DirectiveModule } from '@eas-directives/directive.module';
+import { ZoneServiceModule } from '@eas-services/zone/zone-service.module';
+import { PaginationServiceModule } from '@eas-services/pagination/pagination-service.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    DirectiveModule
+    DirectiveModule,
+    ZoneServiceModule,
+    PaginationServiceModule
   ],
   declarations: [
     FeederAddComponent, 
