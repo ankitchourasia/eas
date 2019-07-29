df35bb36fddf85555b9135aeaf086d5d23e271df
unknown <manishkushwaha07@gmail.com>
2019-07-29 18:05:57 +0530

    export point read added successfully

---
+++ src/app/modules/admin/admin-export/admin-export-point-reading-add/admin-export-point-reading-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'eas-admin-export-point-reading-add',
+  templateUrl: './admin-export-point-reading-add.component.html',
+  styleUrls: ['./admin-export-point-reading-add.component.css']
+})
+export class AdminExportPointReadingAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
