4c779f799faa3c3d6de79601451750bb6f9ed9a7
unknown <manishkushwaha07@gmail.com>
2020-02-22 11:49:37 +0530

    use bs classes

--- src/assets/js/css-variable.js
+++ src/assets/js/css-variable.js
@@ -1,9 +1,4 @@
 //Color     HEX	    GB
-// primary  #0275d8	rgb(2, 117, 216)
-// success  #5cb85c	rgb(92, 184, 92)
-// info     #5bc0de	rgb(91, 192, 222)
-// warning  #f0ad4e	rgb(240, 173, 78)
-// danger   #d9534f	rgb(217, 83, 79)
 // dark     #343a40	rgb(41, 43, 44)
 // default  #f7f7f7	rgb(247, 247, 247)
 
--- src/assets/js/external.js
+++ src/assets/js/external.js
@@ -1,7 +1,10 @@
-$(document).ready(function () {
-  $('#sidebarCollapse').on('click', function () {
-    $('#sidebar').toggleClass('active');
-  });
+$(window).on('resize', function () {
+  var navbarHeaderHeight = $("#navbarHeader").innerHeight();
+  var horizontalNavBarHeight = $("#horizontalNavBar").innerHeight();
+  // $("#verticalNavBar").height(vheight - navbarHeaderHeight - horizontalNavBarHeight);
+  // $("#mainContent").height(vheight - navbarHeaderHeight - horizontalNavBarHeight);
+  document.documentElement.style.setProperty('--header-navbar-height', navbarHeaderHeight + 'px');
+  document.documentElement.style.setProperty('--horizontal-navbar-height', horizontalNavBarHeight + 'px');
 });
 
 $(document).ready(function () {
